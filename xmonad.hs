b610f6132d4140f9f3e2c34c81cea28a0ff48b3f
Ben Gamari <bgamari.foss@gmail.com>
2013-10-30 20:54:53 -0400

    Move to taffybar

--- xmonad.hs
+++ xmonad.hs
@@ -5,7 +5,7 @@ import Control.Monad.Trans.Class (lift)
 import Control.Monad.IO.Class
 import Control.Error
 import System.Exit
- 
+
 import Graphics.X11.ExtraTypes.XF86
 
 import qualified XMonad.StackSet as W
@@ -26,90 +26,93 @@ import XMonad.Actions.CopyWindow (copy)
 import XMonad.Actions.CycleRecentWS (cycleRecentWS)
 import XMonad.Actions.CycleWindows (cycleRecentWindows)
 import XMonad.Actions.CycleWS
-       
+
+import System.Taffybar.XMonadLog
 import MPRIS2
 import TrackPlayers
 import qualified PulseAudio as PA
- 
+import DBus
+import DBus.Client
+
 myTerminal      = "gnome-terminal"
- 
+
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
- 
+
 myBorderWidth   = 1
- 
+
 myModMask       = mod4Mask
- 
+
 myWorkspaces    = ["1","2","3","4","mail","irc"]
- 
+
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#0033ff"
-       
-myXPConfig :: XPConfig              
+
+myXPConfig :: XPConfig
 myXPConfig = def { font = "xft:Ubuntu Mono-10" }
- 
+
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
- 
+
     -- launch a terminal
     [ ((modm              , xK_F1    ), spawn $ XMonad.terminal conf)
 
     -- launch browser
     , ((modm              , xK_F2    ), spawn "firefox")
-    
+
     , ((modm              , xK_F4    ), spawn "emacsclient -c ~")
     , ((modm              , xK_F5    ), spawn "emacsclient -c -e \"(notmuch)\"")
 
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
- 
+
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
- 
+
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
- 
+
      -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
- 
+
     --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
+
     -- Resize viewed windows to the correct size
     , ((modm,               xK_r     ), refresh)
- 
+
     -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
- 
+
     -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
- 
+
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
- 
+
     -- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
- 
+
     -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
- 
+
     -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
     -- Switch to urgent window
     , ((modm,               xK_u     ), focusUrgent )
- 
+
     -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
- 
+
     -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
- 
+
     -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
- 
+
     -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
- 
+
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
@@ -117,13 +120,13 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_equal ), increaseLimit)
     -- Decrease the window limit
     , ((modm              , xK_minus ), decreaseLimit)
- 
+
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
     --
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-    
+
     -- Remove workspace
     , ((modm              , xK_BackSpace), removeWorkspace)
     -- Select workspace
@@ -138,7 +141,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_Tab      ), cycleRecentWS [xK_Super_L] xK_Tab xK_grave)
     -- Cycle through recent windows
     , ((mod1Mask          , xK_Tab      ), cycleRecentWindows [xK_Alt_L] xK_Tab xK_grave)
-    
+
     -- Next workspace
     , ((modm              , xK_Right    ), moveTo Next HiddenNonEmptyWS)
     -- Previous workspace
@@ -150,45 +153,45 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Set workspace directory
     , ((modm              , xK_d        ), changeDir myXPConfig)
-    
+
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
- 
+
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
     ]
     ++
- 
+
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
     [((m .|. modm, k), withNthWorkspace f i)
         | (i, k) <- zip [0..] [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
- 
+
 
 ------------------------------------------------------------------------
 -- Mouse bindings: default actions bound to mouse events
 --
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
- 
+
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
                                        >> windows W.shiftMaster))
- 
+
     -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
- 
+
     -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
                                        >> windows W.shiftMaster))
- 
+
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
- 
+
 ------------------------------------------------------------------------
 -- Layouts:
- 
+
 -- You can specify and transform your layouts by modifying these values.
 -- If you change layout bindings be sure to use 'mod-shift-space' after
 -- restarting (with 'mod-q') to reset your layout state to the new
@@ -209,19 +212,19 @@ myLayout = workspaceDir "/home/ben"
   where
     -- default tiling algorithm partitions the screen into two panes
     tiled   = Tall nmaster delta ratio
- 
+
     -- The default number of windows in the master pane
     nmaster = 1
- 
+
     -- Default proportion of screen occupied by master pane
     ratio   = 1/2
- 
+
     -- Percent of screen to increment by when resizing panes
     delta   = 3/100
- 
+
 ------------------------------------------------------------------------
 -- Window rules:
- 
+
 -- Execute arbitrary actions and WindowSet manipulations when managing
 -- a new window. You can use this to, for example, always float a
 -- particular program, or have a client always appear on a particular
@@ -244,10 +247,10 @@ myManageHook = composeAll
     , className =? "rhythmbox"      --> doShift "music"
     ]
 
- 
+
 ------------------------------------------------------------------------
 -- Event handling
- 
+
 -- Defines a custom handler function for X Events. The function should
 -- return (All True) if the default handler is to be run afterwards. To
 -- combine event hooks use mappend or mconcat from Data.Monoid.
@@ -258,10 +261,10 @@ myManageHook = composeAll
 -- combining them with ewmhDesktopsEventHook.
 --
 myEventHook = mempty
- 
+
 ------------------------------------------------------------------------
 -- Startup hook
- 
+
 -- Perform an arbitrary action each time xmonad starts or is restarted
 -- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
 -- per-workspace layout choices.
@@ -275,17 +278,15 @@ myEventHook = mempty
 --
 myStartupHook = return ()
 
-myXmobarPP = xmobarPP { ppUrgent = xmobarColor "yellow" "red" . xmobarStrip }
-
 -- | Run an EitherT action producing output on error
 noteEitherT :: MonadIO m => EitherT String m a -> m (Maybe a)
 noteEitherT action = runEitherT action >>= either f (return . Just)
   where
     f err = do liftIO $ putStrLn $ "error: "++err
                return Nothing
-      
+
 setupVolumeKeys :: EitherT String IO (M.Map (ButtonMask, KeySym) (X ()))
-setupVolumeKeys = do                
+setupVolumeKeys = do
     pulse <- PA.connect
     device <- PA.getDevices pulse >>= tryHead "No PulseAudio devices"
     return $ M.fromList
@@ -296,8 +297,8 @@ setupVolumeKeys = do
       , ( (0, xF86XK_AudioMute)
         , void $ io $ noteEitherT $ PA.toggleDeviceMute pulse device )
       ]
-   
-setupMediaKeys :: EitherT String IO (M.Map (ButtonMask, KeySym) (X ())) 
+
+setupMediaKeys :: EitherT String IO (M.Map (ButtonMask, KeySym) (X ()))
 setupMediaKeys = do
     session <- liftIO connectSession
     playerList <- trackPlayers session
@@ -312,14 +313,15 @@ setupMediaKeys = do
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
- 
+
 -- Run xmonad with the settings you specify. No need to modify this.
 --
 main = do
     spawn "/usr/bin/xcompmgr -n"
-    xmproc <- spawnPipe "/home/ben/.cabal/bin/xmobar /home/ben/.xmobarrc"
+    spawn "/home/ben/.cabal/bin/taffybar"
     volumeKeys <- runEitherT setupVolumeKeys >>= either (\err->print ("no volume keys: "++err) >> return M.empty) return
     mediaKeys <- runEitherT setupMediaKeys >>= either (\err->print ("no media keys: "++err) >> return M.empty) return
+    dbus <- connectSession
 
     xmonad
       $ withUrgencyHook NoUrgencyHook
@@ -332,18 +334,24 @@ main = do
         workspaces         = myWorkspaces,
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
- 
+
       -- key bindings
         keys               = \c->myKeys c <> mediaKeys <> volumeKeys,
         mouseBindings      = myMouseBindings,
- 
+
       -- hooks, layouts
         layoutHook         = avoidStruts $ myLayout,
         manageHook         = manageDocks <+> myManageHook,
         handleEventHook    = myEventHook,
-        logHook            = dynamicLogWithPP myXmobarPP
-                                 { ppOutput = hPutStrLn xmproc
-                                 , ppTitle = xmobarColor "green" "" . shorten 50
+        logHook            = dbusLogWithPP dbus $ taffybarPP
+                                 { ppUrgent = taffybarColor "black" "#e6abab" . xmobarStrip
+                                 , ppCurrent = taffybarBold . taffybarColor "#00900d" "" . xmobarStrip
+                                 , ppSep = replicate 8 ' '
+                                 , ppWsSep = "  "
                                  },
         startupHook        = myStartupHook
     }
+
+
+taffybarBold :: String -> String
+taffybarBold = wrap "<span weight=\"bold\">" "</span>"
