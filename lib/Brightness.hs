e9c644d7033ff1b535e601b160671ecc0fc58384
Ben Gamari <bgamari.foss@gmail.com>
2013-12-07 17:00:36 -0500

    Fix brightness control

--- lib/Brightness.hs
+++ lib/Brightness.hs
@@ -15,3 +15,5 @@ modifyBrightness client dir =
                         Down -> "StepDown"
     in void $ fmapLT show $ EitherT $ call client
        $ (methodCall "/org/gnome/SettingsDaemon/Power" "org.gnome.SettingsDaemon.Power.Screen" m)
+         { methodCallDestination = Just "org.gnome.SettingsDaemon.Power" }
+
