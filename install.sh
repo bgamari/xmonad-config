ea419fea4b4dbd2fc6b90e7c7bad08fd4aba6c41
Ben Gamari <bgamari.foss@gmail.com>
2014-07-09 21:03:01 -0400

    Install display manager session

--- install.sh
+++ install.sh
@@ -4,3 +4,14 @@ ln -s .xmonad/xsession ~/.xsession
 
 mkdir ~/.config
 ln -s $HOME/.xmonad/taffybar $HOME/.config/taffybar
+
+session=/usr/share/xsession/custom.desktop
+if [ ! -f $session ]; then
+echo "Installing display manager session"
+sudo cat <<EOF >$session
+[Desktop Entry]
+Name=Xsession
+Exec=/etc/X11/Xsession
+EOF
+fi
+
