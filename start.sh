e608f23ddb41b92f24529deede9ddf74e33d6392
Ben Gamari <bgamari.foss@gmail.com>
2014-07-09 21:05:08 -0400

    Add start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#gnome-power-manager &
+
+export UBUNTU_MENUPROXY=
+PATH=$HOME/.cabal/bin:$PATH
+
+$HOME/.env/bin/set-synaptics
+start-pulseaudio-x11
+gnome-settings-daemon &
+nm-applet &
+blueman-applet &
+xmonad
+
