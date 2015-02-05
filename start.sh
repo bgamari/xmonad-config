3aac4c58cf8e3f152e1f55c5744af6de4509c1b8
Ben Gamari <ben@smart-cactus.org>
2015-02-05 17:05:19 -0500

    Switch to xfsettingsd

--- start.sh
+++ start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#gnome-power-manager &
-
 export UBUNTU_MENUPROXY=
 PATH=$HOME/.cabal/bin:$PATH
 
 $HOME/.env/bin/set-synaptics
 start-pulseaudio-x11
-#gnome-settings-daemon &
+xfsettingsd &
 nm-applet &
 blueman-applet &
 xmonad
