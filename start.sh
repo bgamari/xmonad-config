4e5cbf5efd46eb4daa001db68333da6f77296417
Ben Gamari <ben@smart-cactus.org>
2015-11-24 18:01:33 +0100

    Start gpg-agent

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ $HOME/.env/bin/set-synaptics
 start-pulseaudio-x11
 gnome-settings-daemon &
 nm-applet --sm-disable &
+eval $(gpg-agent --daemon)
 system-config-printer-applet &
 blueman-applet &
 xdg-user-dirs-gtk-update
